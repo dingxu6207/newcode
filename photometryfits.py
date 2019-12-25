01740fe1ca2f2f433299a784d51d43eb1507cd5b
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2019-12-25 11:29:13 +0800

    根据目标自动找出测光结果

--- photometryfits.py
+++ photometryfits.py
@@ -14,11 +14,10 @@ from astropy.stats import sigma_clipped_stats
 from photutils import CircularAperture, CircularAnnulus
 from photutils import aperture_photometry
 
-fitsname1 = 'E:\\BOOTES4\\20181118\\03095\\'+'20181118130330-893-RA.fits'
+fitsname1 = 'E:\\BOOTES4\\03088\\'+'test.fits'
 
 onehdu = fits.open(fitsname1)
 imgdata1 = onehdu[0].data  #hdu[0].header
-
 
 def adjustimage(imagedata, coffe):
     mean = np.mean(imagedata)
@@ -37,12 +36,12 @@ def displayimage(img, coff, i):
     minimg,maximg = adjustimage(img, coff)
     plt.figure(i)
     plt.imshow(img, cmap='gray', vmin = minimg, vmax = maximg)
-    #plt.plot(705.918, 510.251, '*')
+    #plt.plot(376.82, 137.232, '*')
     plt.savefig(str(i)+'.jpg')
 
 def findsource(img):    
     mean, median, std = sigma_clipped_stats(img, sigma=3.0) 
-    daofind = DAOStarFinder(fwhm=5.0, threshold=4.*std)
+    daofind = DAOStarFinder(fwhm=5.5, threshold=4.*std)
     sources = daofind(img - median)
     positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
 
@@ -52,12 +51,12 @@ def photometryimg(positions, img, i):
     
     positionslist = positions.tolist()
     
-    aperture = CircularAperture(positionslist, r=12) #2*FWHM
-    annulus_aperture = CircularAnnulus(positionslist, r_in=18, r_out=23)#4*FWHM+2*FWHM
+    aperture = CircularAperture(positionslist, r=11) #2*FWHM
+    annulus_aperture = CircularAnnulus(positionslist, r_in=21, r_out=32)#4-5*FWHM+2*FWHM
     apers = [aperture, annulus_aperture]
     
     displayimage(img,3,i)
-    aperture.plot(color='blue', lw=0.2)
+    aperture.plot(color='blue', lw=0.5)
     annulus_aperture.plot(color='red', lw=0.2)
     
     phot_table = aperture_photometry(img, apers)
@@ -68,6 +67,14 @@ def photometryimg(positions, img, i):
     posflux = np.column_stack((positions, phot_table['residual_aperture_sum']))  
     return posflux
 
+def sourcephotometry(targetx, targety, posflux, threshold=10):
+    hang,lie = sumpho.shape    
+    for i in range(hang):
+        delt = np.sqrt((targetx - sumpho[i][0])**2+(targety - sumpho[i][1])**2)
+        if delt < threshold:
+            print(sumpho[i])
+            mag = 25 - 2.5*np.log10(sumpho[i][2]) #125717
+            print(mag)
 
 displayimage(imgdata1,3,0)
 positions1 = findsource(imgdata1)
@@ -75,6 +82,10 @@ apertures1 = CircularAperture(positions1, r=15.)
 apertures1.plot(color='blue', lw=1.5, alpha=0.5)
 
 sumpho = photometryimg(positions1, imgdata1, 1)
-minposition = np.where(sumpho[:,2] == np.max(sumpho[:,2]))
-mag = 25 - 2.5*np.log10(sumpho[:,2][minposition])
-print(mag)
+
+sourcephotometry(377.88,138.09,sumpho)
+
+
+
+            
+            
