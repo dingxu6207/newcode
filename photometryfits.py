2650807fadcdb29400ddaf3c2ebc93f2dfca0cd5
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2019-12-26 00:04:20 +0800

    测光和画半高全宽

--- photometryfits.py
+++ photometryfits.py
@@ -14,10 +14,11 @@ from astropy.stats import sigma_clipped_stats
 from photutils import CircularAperture, CircularAnnulus
 from photutils import aperture_photometry
 
-fitsname1 = 'E:\\BOOTES4\\03088\\'+'test.fits'
+fitsname1 = 'E:\\BOOTES4\\20190606\\'+'201906061532290716.fit'
 
 onehdu = fits.open(fitsname1)
 imgdata1 = onehdu[0].data  #hdu[0].header
+#obserdata = onehdu[0].header['DATE']
 
 def adjustimage(imagedata, coffe):
     mean = np.mean(imagedata)
@@ -41,7 +42,7 @@ def displayimage(img, coff, i):
 
 def findsource(img):    
     mean, median, std = sigma_clipped_stats(img, sigma=3.0) 
-    daofind = DAOStarFinder(fwhm=5.5, threshold=4.*std)
+    daofind = DAOStarFinder(fwhm=8, threshold=5.*std)
     sources = daofind(img - median)
     positions = np.transpose((sources['xcentroid'], sources['ycentroid']))
 
@@ -51,11 +52,11 @@ def photometryimg(positions, img, i):
     
     positionslist = positions.tolist()
     
-    aperture = CircularAperture(positionslist, r=11) #2*FWHM
-    annulus_aperture = CircularAnnulus(positionslist, r_in=21, r_out=32)#4-5*FWHM+2*FWHM
+    aperture = CircularAperture(positionslist, r=14) #2*FWHM
+    annulus_aperture = CircularAnnulus(positionslist, r_in=32, r_out=48)#4-5*FWHM+2*FWHM
     apers = [aperture, annulus_aperture]
     
-    displayimage(img,3,i)
+    displayimage(img,3,i) ###画图1
     aperture.plot(color='blue', lw=0.5)
     annulus_aperture.plot(color='red', lw=0.2)
     
@@ -67,25 +68,39 @@ def photometryimg(positions, img, i):
     posflux = np.column_stack((positions, phot_table['residual_aperture_sum']))  
     return posflux
 
-def sourcephotometry(targetx, targety, posflux, threshold=10):
+def sourcephotometry(targetx, targety, sumpho, threshold=10):
     hang,lie = sumpho.shape    
     for i in range(hang):
         delt = np.sqrt((targetx - sumpho[i][0])**2+(targety - sumpho[i][1])**2)
         if delt < threshold:
             print(sumpho[i])
             mag = 25 - 2.5*np.log10(sumpho[i][2]) #125717
-            print(mag)
+            #print(mag)
+            return sumpho[i],mag
 
-displayimage(imgdata1,3,0)
+def FWHMplot(x0,y0,width,i):
+    x0 = int(x0)
+    y0 = int(y0)
+    pixlist = []
+    for i in range((y0-width),(y0+width)):
+        pixlist.append(imgdata1[x0,i])
+    
+    plt.figure(i)
+    plt.plot(pixlist)
+    plt.grid(color='r',linestyle='--',linewidth=2)
+    
+    
+displayimage(imgdata1,3,0)  #画图0
 positions1 = findsource(imgdata1)
 apertures1 = CircularAperture(positions1, r=15.)
 apertures1.plot(color='blue', lw=1.5, alpha=0.5)
 
-sumpho = photometryimg(positions1, imgdata1, 1)
+posflux = photometryimg(positions1, imgdata1, 1)
 
-sourcephotometry(377.88,138.09,sumpho)
+posflux1,mag1 = sourcephotometry(572,221,posflux)
+posflux2,mag2 = sourcephotometry(333,141,posflux)
 
+print(mag1,mag2)
 
-
-            
-            
+FWHMplot(posflux2[1],posflux2[0],14,2) #画图2
+         
